a7a68c66802f1a819ec1a892e0a900687aef1f3f
xueqian <xueqian0321@126.com>
2014-09-17 14:28:38 +0800

    consummate test -- index-spec.js

--- test/spec/controllers/index-spec.js
+++ test/spec/controllers/index-spec.js
@@ -25,77 +25,62 @@ describe("indexCtrl", function () {
     });
   });
 
-  it('parent_totalCount', function () {
-
+  it('parent_totalCount should return correct value', function () {
     spyOn(GoodsItemService, 'getTotalCount').and.returnValue(2);
-
     createController();
     $scope.$digest();
-
     $rootScope.$broadcast('parent_totalCount');
     $scope.$digest();
-
     expect($scope.totalCount).toBe(2);
-
   });
 
-  it('parent_totalCount===0', function () {
-
+  it('parent_totalCount should should call getTotalCount in GoodsItemService', function () {
+    spyOn(GoodsItemService, 'getTotalCount');
     createController();
     $scope.$digest();
+    $rootScope.$broadcast('parent_totalCount');
+    $scope.$digest();
+    expect(GoodsItemService.getTotalCount).toHaveBeenCalled();
+  });
 
+  it('parent_totalCount===0 should return correct value', function () {
+    createController();
+    $scope.$digest();
     $rootScope.$broadcast('parent_totalCount===0');
     $scope.$digest();
-
     expect($scope.totalCount).toBe(0);
-
   });
 
-  it('parent_indexActive', function () {
-
+  it('parent_indexActive should return correct value', function () {
     createController();
     $scope.$digest();
-
     $rootScope.$broadcast('parent_indexActive');
     $scope.$digest();
-
     expect($scope.indexActive).toBe(true);
-
   });
 
-  it('parent_goodsListActive', function () {
-
+  it('parent_goodsListActive should return correct value', function () {
     createController();
     $scope.$digest();
-
     $rootScope.$broadcast('parent_goodsListActive');
     $scope.$digest();
-
     expect($scope.goodsListActive).toBe(true);
-
   });
 
-  it('parent_cartActive', function () {
-
+  it('parent_cartActive should return correct value', function () {
     createController();
     $scope.$digest();
-
     $rootScope.$broadcast('parent_cartActive');
     $scope.$digest();
-
     expect($scope.cartActive).toBe(true);
-
   });
 
-  it('parent_manageActive', function () {
-
+  it('parent_manageActive should return correct value', function () {
     createController();
     $scope.$digest();
-
     $rootScope.$broadcast('parent_manageActive');
     $scope.$digest();
-
     expect($scope.manageActive).toBe(true);
-
   });
+
 });
