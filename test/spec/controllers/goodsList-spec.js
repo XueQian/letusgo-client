8ef2b5fdb85735e78bf4130265f421f1705de15e
xueqian <xueqian0321@126.com>
2014-09-17 14:10:18 +0800

    consummate test -- goodsList-spec.js

--- test/spec/controllers/goodsList-spec.js
+++ test/spec/controllers/goodsList-spec.js
@@ -27,72 +27,111 @@ describe("goodsListCtrl", function () {
     });
   });
 
-  it('$emit correct', function () {
-    spyOn($scope, '$emit');
-    createController();
-    expect($scope.$emit).toHaveBeenCalledWith('parent_totalCount');
-    expect($scope.$emit).toHaveBeenCalledWith('parent_goodsListActive');
-
-  });
-
-  describe('itemList operate', function () {
-
-    var itemList;
-
-    beforeEach(function () {
-
-      itemList = [
-        {barcode: 'ITEM00000', 'category': '服装鞋包', name: '服装１', 'price': 11, 'unit': '件'}
-      ];
-
-      spyOn(GoodsItemService, 'get').and.returnValue(itemList);
+  describe('when load', function () {
+    it('it should emit to parent_goodsListActive', function () {
+      spyOn($scope, '$emit');
       createController();
+      expect($scope.$emit).toHaveBeenCalledWith('parent_goodsListActive');
     });
 
-    it('products correctly', function () {
-
-      expect($scope.products[0].name).toEqual('服装１');
+    it('it should emit to parent_totalCount', function () {
+      spyOn($scope, '$emit');
+      createController();
+      expect($scope.$emit).toHaveBeenCalledWith('parent_totalCount');
     });
 
-  });
+    it('should call loadItems in GoodsItemService', function () {
+      spyOn(GoodsItemService, 'loadItems');
+      createController();
+      expect(GoodsItemService.loadItems).toHaveBeenCalled();
+    });
 
-  describe('when addToCart() ', function () {
+    it('should call get in GoodsItemService 2 times', function () {
+      spyOn(GoodsItemService, 'get');
+      createController();
+      expect(GoodsItemService.get).toHaveBeenCalled();
+      expect(GoodsItemService.get.calls.count()).toBe(2);
+    });
 
-    it('use localStorageService .set', function () {
-      var productItem = {barcode: 'ITEM00002', category: '手机数码', name: '手机１', price: 1111, unit: '件'};
-
+    it('should call set in GoodsItemService 2 times', function () {
       spyOn(GoodsItemService, 'set');
       createController();
-      $scope.addToCart(productItem);
-
-      expect(GoodsItemService.set.calls.count()).toEqual(4);
-
+      expect(GoodsItemService.set).toHaveBeenCalled();
+      expect(GoodsItemService.set.calls.count()).toBe(2);
     });
 
-    it('addToCart  is ok', function () {
-
-      var productItem = {};
-
-      var cartList = null;
-
-      spyOn(GoodsItemService, 'get').and.returnValue(cartList);
-
+    it('should call getTotalCount in GoodsItemService', function () {
+      spyOn(GoodsItemService, 'getTotalCount');
       createController();
-      $scope.addToCart(productItem);
-
-      expect(GoodsItemService.getTotalCount(cartList)).toBe(0);
+      expect(GoodsItemService.getTotalCount).toHaveBeenCalled();
     });
 
-    it('getCategoryName  is ok', function () {
+  });
+
+  describe('when addToCart', function () {
+
+    it('getCategoryName should return correct val', function () {
       var id = 0;
       var result = {id: 0, name: '服装鞋包'};
       spyOn(Operatecategorieservice, 'getcategoryById').and.returnValue(result);
-
       createController();
       expect($scope.getCategoryName(id)).toBe('服装鞋包');
     });
 
   });
+
+  describe('when addToCart', function () {
+    var itemList;
+
+    beforeEach(function () {
+      itemList = [
+        {barcode: 'ITEM00000', 'category': '服装鞋包', name: '服装１', 'price': 11, 'unit': '件'}
+      ];
+    });
+
+    it('should call set in GoodsItemService 4 times', function () {
+      var productItem = {barcode: 'ITEM00002', category: '手机数码', name: '手机１', price: 1111, unit: '件'};
+      spyOn(GoodsItemService, 'set');
+      createController();
+      $scope.addToCart(productItem);
+      expect(GoodsItemService.set).toHaveBeenCalled();
+      expect(GoodsItemService.set.calls.count()).toBe(4);
+    });
+
+    it('should call addToCartList in GoodsItemService', function () {
+      var productItem = {barcode: 'ITEM00002', category: '手机数码', name: '手机１', price: 1111, unit: '件'};
+      spyOn(GoodsItemService, 'addToCartList');
+      createController();
+      $scope.addToCart(productItem);
+      expect(GoodsItemService.addToCartList).toHaveBeenCalled();
+    });
+
+    it('it should emit to parent_totalCount 3 times', function () {
+      var productItem = {barcode: 'ITEM00002', category: '手机数码', name: '手机１', price: 1111, unit: '件'};
+      spyOn($scope, '$emit');
+      createController();
+      $scope.addToCart(productItem);
+      expect($scope.$emit).toHaveBeenCalledWith('parent_totalCount');
+      expect($scope.$emit.calls.count()).toBe(3);
+    });
+
+    it('products should return correct value', function () {
+      spyOn(GoodsItemService, 'get').and.returnValue(itemList);
+      createController();
+      expect($scope.products[0].name).toEqual('服装１');
+    });
+
+    it('cartList=null,should return correct value', function () {
+      var productItem = {};
+      var cartList = null;
+      spyOn(GoodsItemService, 'get').and.returnValue(cartList);
+      createController();
+      $scope.addToCart(productItem);
+      expect(GoodsItemService.getTotalCount(cartList)).toBe(0);
+    });
+
+  });
+
 });
 
 
