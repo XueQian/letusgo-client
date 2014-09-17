8294486b375922c44d61d006c963b096bcbffeab
xueqian <xueqian0321@126.com>
2014-09-17 15:40:43 +0800

    consummate test -- modifyGoodsItems-spec.js

--- test/spec/controllers/modifyGoodsItems-spec.js
+++ test/spec/controllers/modifyGoodsItems-spec.js
@@ -30,35 +30,48 @@ describe("modifyGoodsItemsCtrl", function () {
     });
   });
 
-  it('$emit', function () {
-    spyOn($scope, '$emit');
-    createController();
-    expect($scope.$emit).toHaveBeenCalledWith('parent_manageGoodsActive');
+  describe('when load', function () {
+
+    it('it should emit to parent_manageGoodsActive', function () {
+      spyOn($scope, '$emit');
+      createController();
+      expect($scope.$emit).toHaveBeenCalledWith('parent_manageGoodsActive');
+    });
+
+    it('should call loadcategories in Operatecategorieservice', function () {
+      spyOn(Operatecategorieservice, 'loadcategories');
+      createController();
+      expect(Operatecategorieservice.loadcategories).toHaveBeenCalled();
+    });
+
+    it('getGoodsItemsByBarcode,categories and category should return correct value', function () {
+      spyOn(Operategoodsitemservice, 'getGoodsItemsByBarcode').and.returnValue({barcode: 'ITEM00005', category: '1', name: '用品１', price: 11, unit: '件'});
+      spyOn(Operatecategorieservice, 'loadcategories').and.returnValue([
+        {id: 1, name: '测试1'},
+        {id: 2, name: '测试2'}
+      ]);
+      createController();
+      expect($scope.itemList.name).toBe('用品１');
+      expect($scope.categories[0].name).toBe('测试1');
+      expect($scope.category.name).toBe('测试1');
+    });
+
   });
 
-  it('getGoodsItemsByBarcode,categories and category', function () {
-    spyOn(Operategoodsitemservice, 'getGoodsItemsByBarcode').and.returnValue({barcode: 'ITEM00005', category: '1', name: '用品１', price: 11, unit: '件'});
-    spyOn(Operatecategorieservice, 'loadcategories').and.returnValue([
-      {id: 1, name: '测试1'},
-      {id: 2, name: '测试2'}
-    ]);
+  describe('when modifyGoods', function () {
 
-    createController();
-    expect($scope.itemList.name).toBe('用品１');
-    expect($scope.categories[0].name).toBe('测试1');
-    expect($scope.category.name).toBe('测试1');
-  });
+    it('should call modifyGoods in Operategoodsitemservice', function () {
+      spyOn(Operategoodsitemservice, 'getGoodsItemsByBarcode').and.returnValue({barcode: 'ITEM00005', category: '1', name: '用品１', price: 11, unit: '件'});
+      spyOn(Operatecategorieservice, 'loadcategories').and.returnValue([
+        {id: 1, name: '测试1'},
+        {id: 2, name: '测试2'}
+      ]);
+      spyOn(Operategoodsitemservice, 'modifyGoods');
+      createController();
+      $scope.modifyGoods();
+      expect(Operategoodsitemservice.modifyGoods).toHaveBeenCalled();
+    });
 
-  it('modifyGoods', function () {
-    spyOn(Operategoodsitemservice, 'getGoodsItemsByBarcode').and.returnValue({barcode: 'ITEM00005', category: '1', name: '用品１', price: 11, unit: '件'});
-    spyOn(Operatecategorieservice, 'loadcategories').and.returnValue([
-      {id: 1, name: '测试1'},
-      {id: 2, name: '测试2'}
-    ]);
-    spyOn(Operategoodsitemservice, 'modifyGoods');
-    createController();
-    $scope.modifyGoods();
-    expect(Operategoodsitemservice.modifyGoods).toHaveBeenCalled();
   });
 
 });
