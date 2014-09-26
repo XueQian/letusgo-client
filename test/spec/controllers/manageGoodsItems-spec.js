cb8d69a58c64fab5efc7348c1b8338b04c3a13f5
Xue Qian <xueqian0321@126.com>
2014-09-26 23:54:18 +0800

    test manageGoodsItems.js 100%

--- test/spec/controllers/manageGoodsItems-spec.js
+++ test/spec/controllers/manageGoodsItems-spec.js
@@ -1,108 +1,154 @@
-//'use strict';
-//
-//describe("manageGoodsItemsCtrl", function () {
-//
-//  var createController, $scope, GoodsItemService, Operatecategorieservice, Operategoodsitemservice, localStorageService;
-//
-//  beforeEach(function () {
-//
-//    module('letusgoApp');
-//
-//    inject(function ($injector) {
-//
-//      $scope = $injector.get('$rootScope').$new();
-//      GoodsItemService = $injector.get('GoodsItemService');
-//      Operatecategorieservice = $injector.get('Operatecategorieservice');
-//      Operategoodsitemservice = $injector.get('Operategoodsitemservice');
-//      localStorageService = $injector.get('localStorageService');
-//      var $controller = $injector.get('$controller');
-//
-//      createController = function () {
-//
-//        return $controller('manageGoodsItemsCtrl', {
-//          $scope: $scope,
-//          GoodsItemService: GoodsItemService,
-//          Operatecategorieservice: Operatecategorieservice,
-//          Operategoodsitemservice: Operategoodsitemservice,
-//          localStorageService: localStorageService
-//
-//        });
-//      };
-//    });
-//  });
-//
-//  describe('when load', function () {
-//
-//    it('it should emit to parent_manageActive', function () {
-//      spyOn($scope, '$emit');
-//      createController();
-//      expect($scope.$emit).toHaveBeenCalledWith('parent_manageActive');
-//    });
-//
-//    it('should call loadGoodsItems in Operategoodsitemservice', function () {
-//      spyOn(Operategoodsitemservice, 'loadGoodsItems');
-//      createController();
-//      expect(Operategoodsitemservice.loadGoodsItems).toHaveBeenCalled();
-//    });
-//
-//    it('should call loadcategories in Operatecategorieservice', function () {
-//      spyOn(Operatecategorieservice, 'loadcategories');
-//      createController();
-//      expect(Operatecategorieservice.loadcategories).toHaveBeenCalled();
-//    });
-//
-//    it('should call loadcategories in GoodsItemService', function () {
-//      spyOn(GoodsItemService, 'get');
-//      createController();
-//      expect(GoodsItemService.get).toHaveBeenCalled();
-//    });
-//
-//  });
-//
-//  describe('when getCategoryName', function () {
-//
-//    it('should return correct value', function () {
-//      var id = 0;
-//      createController();
-//      spyOn(Operatecategorieservice, 'getcategoryById').and.returnValue({id: 0, name: '0'});
-//      $scope.getCategoryName(id);
-//      expect($scope.getCategoryName(id)).toBe('0');
-//    });
-//
-//  });
-//
-//  describe('when deleteCategory', function () {
-//
-//    it('should call set in GoodsItemService and should call splice', function () {
-//      var index = 1;
-//      $scope.products = [
-//        {barcode: 1, name: '测试1'},
-//        {barcode: 2, name: '测试2'}
-//      ];
-//      createController();
-//      spyOn($scope.products, 'splice');
-//      spyOn(GoodsItemService, 'set');
-//      $scope.deleteCategory(index);
-//      expect($scope.products.splice).toHaveBeenCalledWith(index, 1);
-//      expect(GoodsItemService.set).toHaveBeenCalledWith('itemList', $scope.products);
-//
-//    });
-//
-//  });
-//
-//  describe('when deleteCategory', function () {
-//
-//    it('should call addGoodsItems in Operategoodsitemservice', function () {
-//      $scope.item = {};
-//      $scope.itemList = [
-//        {}
-//      ];
-//      createController();
-//      spyOn(Operategoodsitemservice, 'addGoodsItems');
-//      $scope.addGoodsItems();
-//      expect(Operategoodsitemservice.addGoodsItems).toHaveBeenCalledWith($scope.item, $scope.itemList);
-//    })
-//
-//  });
-//
-//});
+'use strict';
+
+describe("manageGoodsItemsCtrl", function () {
+
+  var createController,$location, $scope, Operatecategorieservice, Operategoodsitemservice;
+
+  beforeEach(function () {
+
+    module('letusgoApp');
+
+    inject(function ($injector) {
+
+      $scope = $injector.get('$rootScope').$new();
+      $location = $injector.get('$location');
+      Operatecategorieservice = $injector.get('Operatecategorieservice');
+      Operategoodsitemservice = $injector.get('Operategoodsitemservice');
+      var $controller = $injector.get('$controller');
+
+      createController = function () {
+
+        return $controller('manageGoodsItemsCtrl', {
+          $scope: $scope,
+          Operatecategorieservice: Operatecategorieservice,
+          Operategoodsitemservice: Operategoodsitemservice,
+          $location: $location
+
+        });
+      };
+    });
+  });
+
+  describe('when load', function () {
+
+    it('it should emit to parent_manageActive', function () {
+      spyOn($scope, '$emit');
+      createController();
+      expect($scope.$emit).toHaveBeenCalledWith('parent_manageActive');
+    });
+
+  });
+
+  describe('when getGoodsItems', function () {
+
+    var items;
+
+    beforeEach(function () {
+      items = [
+        {item: {barcode: 'ITEM00000', 'category': '服装鞋包', name: '服装１', 'price': 11, 'unit': '件'}, count: 1}
+      ];
+    });
+
+    it('should return goodsItems', function () {
+
+      spyOn(Operategoodsitemservice, 'getGoodsItems').and.callFake(function (callback) {
+        callback(items);
+      });
+
+      createController();
+
+      Operategoodsitemservice.getGoodsItems(function (data) {
+        expect($scope.products).toEqual(data);
+      });
+
+    });
+  });
+
+  describe('when deleteItems', function () {
+
+    it('should call deleteGoodsItems in Operategoodsitemservice', function () {
+
+      var index = 0;
+      spyOn(Operategoodsitemservice,'deleteGoodsItems');
+
+      createController();
+      $scope.deleteItems(index);
+      expect(Operategoodsitemservice.deleteGoodsItems).toHaveBeenCalledWith(index);
+
+    });
+
+    it('should get goodsItems',function() {
+
+      var items = [
+        {item: {barcode: 'ITEM00000', 'category': '服装鞋包', name: '服装１', 'price': 11, 'unit': '件'}, count: 1}
+      ];
+      var index = 0;
+
+      spyOn(Operategoodsitemservice, 'getGoodsItems').and.callFake(function (callback) {
+        callback(items);
+      });
+
+      createController();
+      $scope.deleteItems(index);
+
+      Operategoodsitemservice.getGoodsItems(function (data) {
+        expect($scope.products).toEqual(data);
+      });
+    });
+
+  });
+
+  describe('when getCategories', function () {
+
+    var categories;
+
+    beforeEach(function () {
+      categories = [
+        {id: 0, name: '服装鞋包'}
+      ];
+    });
+
+    it('should return items to cart', function () {
+
+      spyOn(Operatecategorieservice, 'getCategories').and.callFake(function (callback) {
+        callback(categories);
+      });
+
+      createController();
+
+      Operatecategorieservice.getCategories(function (data) {
+        expect($scope.categories).toEqual(data);
+      });
+
+    });
+
+  });
+
+  describe('when addGoodsItems', function () {
+
+    var items,item;
+
+    beforeEach(function () {
+      items = [
+        {item: {barcode: 'ITEM00000', 'category': '服装鞋包', name: '服装１', 'price': 11, 'unit': '件'}, count: 1}
+      ];
+      item={barcode: 'ITEM00000', 'category': '服装鞋包', name: '服装１', 'price': 11, 'unit': '件'};
+    });
+
+    it('should return items after add', function () {
+
+      spyOn(Operategoodsitemservice, 'addItem').and.callFake(function (item,callback) {
+        callback(items);
+      });
+
+      createController();
+      $scope.addGoodsItems();
+
+      Operategoodsitemservice.addItem(item,function (data) {
+        expect($scope.products).toEqual(data);
+      });
+
+    });
+  });
+
+});
