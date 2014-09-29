bce334dc486fba5699f8e3afbe5ff94790721264
Xue Qian <xueqian0321@126.com>
2014-09-29 20:43:48 +0800

    refactor-extract function EventName at itemList.js

--- app/scripts/controllers/itemList.js
+++ app/scripts/controllers/itemList.js
@@ -3,19 +3,24 @@
 angular.module('letusgoApp')
   .controller('goodsListCtrl', function ($scope, CartService, ItemService) {
 
-    $scope.$emit('parent_itemListActive');
+    function EventName(){
+      this.PARENT_ITEMLIST_ACTIVE='parent_itemListActive';
+      this.PARENT_TOTAL_COUNT='parent_totalCount';
+    }
+
+    $scope.$emit(new EventName().PARENT_ITEMLIST_ACTIVE);
 
     ItemService.getGoodsItems(function (data) {
       $scope.itemList = data;
     });
 
-    $scope.$emit('parent_totalCount');
+    $scope.$emit(new EventName().PARENT_TOTAL_COUNT);
 
     function getTotalCount() {
 
       CartService.getTotalCount($scope.itemList, function (data) {
         CartService.set('totalCount', data);
-        $scope.$emit('parent_totalCount');
+        $scope.$emit(new EventName().PARENT_TOTAL_COUNT);
       });
     }
 
